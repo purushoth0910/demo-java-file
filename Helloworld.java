c5aa95c72af4c7a9773f208ca9b216de9e6ab875
purushoth0910 <purushothkumar.dhoni@gmail.com>
2025-03-14 17:40:57 +0530

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -1,5 +1,6 @@
 public class Helloworld {
   punblic static void main(string[] args) {
-    system.out.println("Hello, world!");
+    system.out.println ("Hello, world!");
+    system.out.println ("Hello every one from helloworld");
   }
 }
